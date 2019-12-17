e87038da3b414f7a29ced261e48c571fded89b2c
경호 최 <rudgh710@naver.com>
2019-12-18 02:11:18 +0900

    HellowPhysX

--- Practice_PhysX/00_HellowPhysX/HellowPhysX.cpp
+++ Practice_PhysX/00_HellowPhysX/HellowPhysX.cpp
@@ -1,5 +1,6 @@
 
 #include <Windows.h>
+#include <ctype.h>
 
 #include "PxPhysicsAPI.h"
 
@@ -25,6 +26,10 @@ PxPvd* gPvd = nullptr;
 
 PxReal stackZ = 10.0f;
 
+PxRigidDynamic* CreateDynamic(const PxTransform& t, const PxGeometry& geometry,
+	const PxVec3& velocity = PxVec3(0));
+void CreateStack(const PxTransform& t, PxU32 size, PxReal halfExtent);
+
 void InitPhysics(bool interactive)
 {
 	gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
@@ -46,22 +51,107 @@ void InitPhysics(bool interactive)
 	sceneDesc.filterShader = PxDefaultSimulationFilterShader;
 
 	gScene = gPhysics->createScene(sceneDesc);
+
+	PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+
+	if (pvdClient)
+	{
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+	}
+
+	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+	PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0, 1, 0, 0), *gMaterial);
+	gScene->addActor(*groundPlane);
+
+	for (PxU32 i = 0; i < 5; i++)
+	{
+		CreateStack(PxTransform(PxVec3(0, 0, stackZ -= 10.0f)), 10, 2.0f);
+	}
+
+	if (!interactive)
+	{
+		CreateDynamic(PxTransform(PxVec3(0, 40, 100)), PxSphereGeometry(10), PxVec3(0, -50, -100));
+	}
 }
 
-PxRigidDynamic* CreateDynamic(const PxTransform& t, const PxGeometry& geometry, const PxVec3& velocity = PxVec3(0))
+
+PxRigidDynamic* CreateDynamic(const PxTransform& t, const PxGeometry& geometry, const PxVec3& velocity)
 {
+	PxRigidDynamic* dynamic = PxCreateDynamic(*gPhysics, t, geometry, *gMaterial, 10.0f);
+	dynamic->setAngularDamping(0.5f);
+	dynamic->setLinearVelocity(velocity);
 
-	//PxRigidDynamic* dynamic= PxCreateDynamic(*gPhysics)
-	return nullptr;
+	gScene->addActor(*dynamic);
+
+	return dynamic;
 }
 
+void CreateStack(const PxTransform& t, PxU32 size, PxReal halfExtent)
+{
+	PxShape* shape = gPhysics->createShape(PxBoxGeometry(halfExtent, halfExtent, halfExtent), *gMaterial);
+
+	for (PxU32 i = 0; i < size; i++)
+	{
+		for (PxU32 j = 0; j < size - i; j++)
+		{
+			PxTransform localTm(PxVec3(PxReal(j * 2) - PxReal(size - i), PxReal(i * 2 + 1), 0) * halfExtent);
+
+			PxRigidDynamic* body = gPhysics->createRigidDynamic(t.transform(localTm));
+			body->attachShape(*shape);
+			PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+			gScene->addActor(*body);
+		}
+	}
+
+	shape->release();
+}
+
+void StepPhysics(bool)
+{
+	gScene->simulate(1.0f / 60.0f);
+	gScene->fetchResults(true);
+}
+
+void CleanupPhysics(bool)
+{
+	PX_RELEASE(gScene);
+	PX_RELEASE(gDispatcher);
+	PX_RELEASE(gPhysics);
+
+	if (gPvd)
+	{
+		PxPvdTransport* transport = gPvd->getTransport();
+		gPvd->release();
+		gPvd = nullptr;
+		PX_RELEASE(transport);
+	}
+	PX_RELEASE(gFoundation);
+}
+
+void KeyPress(unsigned char key, const PxTransform& camera)
+{
+	switch (toupper(key))
+	{
+	case 'B':	CreateStack(PxTransform(PxVec3(0, 0, stackZ -= 10.0f)), 10, 2.0f);						break;
+	case ' ':	CreateDynamic(camera, PxSphereGeometry(3.0f), camera.rotate(PxVec3(0, 0, -1)) * 200);	break;
+	}
+}
 
 int snippetMain(int, const char* const*)
 {
+	
 #ifdef RENDER_SNIPPET
-
+	extern void RenderLoop();
+	RenderLoop();
 #else
-
+	static const PxU32 frameCount = 100;
+	InitPhysics(false);
+	for (PxU32 i = 0; i < frameCount; i++)
+		StepPhysics(false);
+	CleanupPhysics(false);
 #endif
 
 	return 0;
