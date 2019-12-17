511d91d08cac4b2b857be137d832e28ecccb901c
경호 최 <rudgh710@naver.com>
2019-12-17 23:38:23 +0900

    Base

---
+++ Practice_PhysX/HellowPhysX/HellowPhysX.cpp
@@ -0,0 +1,69 @@
+
+#include <ctype.h>
+#include <Windows.h>
+
+#include "PxPhysicsAPI.h"
+
+#include "SnippetPrint.h"
+#include "SnippetPVD.h"
+#include "SnippetUtils.h"
+
+using namespace physx;
+
+PxDefaultAllocator gAllocator;
+
+PxDefaultErrorCallback gErrorCallback;
+
+PxFoundation* gFoundation = nullptr;
+PxPhysics* gPhysics = nullptr;
+
+PxDefaultCpuDispatcher* gDispatcher = nullptr;
+PxScene* gScene = nullptr;
+
+PxMaterial* gMaterial = nullptr;
+
+PxPvd* gPvd = nullptr;
+
+PxReal stackZ = 10.0f;
+
+void InitPhysics(bool interactive)
+{
+	gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+
+	gPvd = PxCreatePvd(*gFoundation);
+	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
+	gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+
+	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
+
+	SYSTEM_INFO info = {};
+	GetSystemInfo(&info);
+
+	gDispatcher = PxDefaultCpuDispatcherCreate(info.dwNumberOfProcessors);
+
+	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+	sceneDesc.cpuDispatcher = gDispatcher;
+	sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+
+	gScene = gPhysics->createScene(sceneDesc);
+}
+
+PxRigidDynamic* CreateDynamic(const PxTransform& t, const PxGeometry& geometry, const PxVec3& velocity = PxVec3(0))
+{
+
+	//PxRigidDynamic* dynamic= PxCreateDynamic(*gPhysics)
+	return nullptr;
+}
+
+
+int snippetMain(int, const char* const*)
+{
+#ifdef RENDER_SNIPPET
+	
+#else
+
+#endif
+	
+	return 0;
+}
